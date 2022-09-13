80e7cd2fc66484e7da0cfa49689fbc1d9c9adf78
JeyPey <tudorkawasaki@gmail.com>
2022-09-13 17:36:53 +0300

    lastCommit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// 1.
+// 1. Create a Basic JavaScript Object
 
 let dog = {
     name: 'Zlata',
@@ -6,7 +6,7 @@ let dog = {
 };
 // ---------------------------------------
 
-// 2.
+// 2. Use Dot Notation to Access the Properties of an Object
 
 let dog = {
     name: "Zlata",
@@ -16,7 +16,7 @@ let dog = {
 console.log(dog.numLegs, dog.name);
 // ---------------------------------------
 
-// 3.
+// 3. Create a Method on an Object
 
 let dog = {
     name: "Zlata",
@@ -28,7 +28,7 @@ let dog = {
 dog.sayLegs();
 // --------------------------------------
 
-// 4.
+// 4. Make Code More Reusable with the this Keyword
 
 let dog = {
     name: "Zlata",
@@ -39,7 +39,7 @@ let dog = {
 dog.sayLegs();
 // --------------------------------------
 
-// 5.
+// 5. Define a Constructor Function
 
 function Dog() {
     this.name = 'Zlata';
@@ -48,7 +48,7 @@ function Dog() {
 }
 // --------------------------------------
 
-// 6.
+// 6. Use a Constructor to Create Objects
 
 function Dog() {
     this.name = "Rupert";
@@ -59,7 +59,7 @@ function Dog() {
 let hound = new Dog();
 // --------------------------------------
 
-// 7.
+// 7. Extend Constructors to Receive Arguments
 
 function Dog(name, color) {
     this.name = name;
@@ -70,7 +70,7 @@ function Dog(name, color) {
 let terrier = new Dog('Jessica', "brown");
 // --------------------------------------
 
-// 8.
+// 8. Verify an Object's Constructor with instanceof
 
 function House(numBedrooms) {
     this.numBedrooms = numBedrooms;
@@ -80,7 +80,7 @@ let myHouse = new House(3);
 myHouse instanceof House;
 // ---------------------------------------
 
-// 9.
+// 9. Understand Own Properties
 
 function Bird(name) {
     this.name = name;
@@ -99,7 +99,7 @@ for (let property in canary) {
 console.log(ownProps);
 // ---------------------------------------
 
-// 10.
+// 10. Use Prototype Properties to Reduce Duplicate Code
 
 function Dog(name) {
     this.name = name;
@@ -108,3 +108,307 @@ Dog.prototype.numLegs = 2;
 
 let beagle = new Dog("Snoopy");
 // ---------------------------------------
+
+// 11. Iterate Over All Properties
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype.numLegs = 4;
+
+let beagle = new Dog("Snoopy");
+
+let ownProps = [];
+let prototypeProps = [];
+
+for (let property in beagle) {
+    if (Dog.hasOwnProperty(property)) {
+        ownProps.push(property);
+    } else {
+        prototypeProps.push(property);
+    }
+}
+// ---------------------------------------
+
+// 12. Understand the Constructor Property
+
+function Dog(name) {
+    this.name = name;
+}
+
+function joinDogFraternity(candidate) {
+    if (candidate.constructor === Dog) {
+        return true;
+    } else {
+        return false;
+    }
+
+}
+// ----------------------------------------
+
+// 13. Change the Prototype to a New Object
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    numLegs: 4,
+    eat: function () {
+        console.log('chomp chomp chomp');
+    },
+    describe: function () {
+        console.log('My name is' + this.name);
+    }
+
+};
+// ---------------------------------------
+
+// 14. Change the Prototype to a New Object
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function () {
+        console.log("chomp chomp chomp");
+    },
+    describe: function () {
+        console.log('My name is ' + this.name);
+    }
+};
+// ---------------------------------------
+
+// 15. Remember to Set the Constructor Property when Changing the Prototype
+
+function Dog(name) {
+    this.name = name;
+}
+
+Dog.prototype = {
+    constructor: Dog,
+    numLegs: 4,
+    eat: function () {
+        console.log("chomp chomp chomp");
+    },
+    describe: function () {
+        console.log('My name is ' + this.name);
+    }
+};
+// ---------------------------------------
+
+// 16. Understand Where an Object’s Prototype Comes From
+
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);
+// ---------------------------------------
+
+// 17. Understand the Prototype Chain
+
+function Dog(name) {
+    this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);
+
+Object.prototype.isPrototypeOf(Dog.prototype);
+// ----------------------------------------
+
+// 18. Use Inheritance So You Don't Repeat Yourself
+
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype = {
+    constructor: Cat,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log(this.name);
+    }
+
+};
+
+Cat.prototype = {
+    constructor: Cat
+};
+
+Bear.prototype = {
+    constructor: Bear
+};
+// ----------------------------------------
+
+// 19. Inherit Behaviors from a Supertype
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
+
+let duck = Object.create(Animal.prototype);
+let beagle = Object.create(Animal.prototype);
+// ----------------------------------------
+
+// 20. Set the Child's Prototype to an Instance of the Parent
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function () {
+        console.log("nom nom nom");
+    }
+};
+
+function Dog() { }
+Dog.prototype = Object.create(Animal.prototype);
+
+let beagle = new Dog();
+// ----------------------------------------
+
+// 21. Reset an Inherited Constructor Property
+
+function Animal() { }
+function Bird() { }
+function Dog() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Dog.prototype = Object.create(Animal.prototype);
+
+Bird.prototype.constructor = Bird;
+Dog.prototype.constructor = Dog;
+
+
+let duck = new Bird();
+let beagle = new Dog();
+// -----------------------------------------
+
+// 22. Add Methods After Inheritance
+
+function Animal() { }
+Animal.prototype.eat = function () { console.log("nom nom nom"); };
+
+function Dog() { }
+
+Dog.prototype = Object.create(Animal.prototype);
+Dog.prototype.constructor = Dog;
+
+Dog.prototype.bark = function () {
+    console.log('Woof!');
+};
+
+
+let beagle = new Dog();
+// -----------------------------------------
+
+// 23. Override Inherited Methods
+
+function Bird() { }
+
+Bird.prototype.fly = function () { return "I am flying!"; };
+
+function Penguin() { }
+Penguin.prototype = Object.create(Bird.prototype);
+Penguin.prototype.constructor = Penguin;
+
+Penguin.prototype.fly = function () {
+    return 'Alas, this is a flightless bird.';
+}
+
+let penguin = new Penguin();
+console.log(penguin.fly());
+// -----------------------------------------
+
+// 24. Use a Mixin to Add Common Behavior Between Unrelated Objects
+
+let bird = {
+    name: "Donald",
+    numLegs: 2,
+};
+
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+
+let glideMixin = function (obj) {
+    obj.glide = function () {
+        console.log('we slide, brbrbrbr');
+    }
+};
+
+glideMixin(bird);
+glideMixin(boat);
+// -----------------------------------------
+
+// 25. Use Closure to Protect Properties Within an Object from Being Modified Externally
+
+function Bird() {
+    let weight = 15;
+
+    this.getWeight = function () {
+        return weight;
+    };
+
+}
+// -----------------------------------------
+
+// 26. Understand the Immediately Invoked Function Expression (IIFE)
+
+(function () {
+    console.log("A cozy nest is ready");
+})();
+// -----------------------------------------
+
+
+// 27. Use an IIFE to Create a Module
+
+let funModule = (function () {
+    return {
+        isCuteMixin: function (obj) {
+            obj.isCute = function () {
+                console.log('very cute mixin <3');
+            };
+        },
+        singMixin: function (obj) {
+            obj.sing = function () {
+                console.log("Singing to an awesome tune");
+            };
+        }
+    }
+})();
+
